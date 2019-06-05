fd66bfa3ec650b0227dea13da3123704638d33ec
Ben Bennett-Cauchon, Daniel Hongel and Micah Zaayer <micah@>
2019-06-05 14:22:52 -0700

    add bootstrap and jquery, as well as adding initial forms, and restructured scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
-var add = function(number1, number2){
-return number1 + number2;
+// Business (or back-end) logic:
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
 };
 
 var number1 = parseInt(prompt("enter a number:"));
@@ -7,27 +20,20 @@ var number2 = parseInt(prompt("enter another number:"));
 var result = add(number1, number2);
 alert(result);
 
-var subtract = function(number1, number2){
-return number1 - number2;
-};
+
 
 var number1 = parseInt(prompt("enter a number:"));
 var number2 = parseInt(prompt("enter another number:"));
 var result = subtract(number1, number2);
 alert(result);
 
-var multiply = function(number1, number2){
-return number1 * number2;
-};
 
 var number1 = parseInt(prompt("enter a number:"));
 var number2 = parseInt(prompt("enter another number:"));
 var result = multiply(number1, number2);
 alert(result);
 
-var divide = function(number1, number2){
-return number1 / number2;
-};
+
 
 var number1 = parseInt(prompt("enter a number:"));
 var number2 = parseInt(prompt("enter another number:"));
